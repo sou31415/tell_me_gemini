aaabfe3b55475a58dcc7b9beab7d47ea226ec09c
sou31415 <tbc3141@icloud.com>
2024-06-24 21:34:24 +0900

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 pub mod model;
 pub mod secret;
-use std::{fs::{File, OpenOptions}, io::{Read, Write}};
+use std::{
+    fs::{File, OpenOptions},
+    io::{Read, Write},
+};
 
-use crate::model::{AppendToFile,Args, ResponseModel, StdPostModel};
+use crate::model::{AppendToFile, Args, Language, ResponseModel, StdPostModel};
 use clap::Parser;
 use reqwest::Client;
 use secret::TOKEN;
@@ -16,7 +19,18 @@ async fn main() -> reqwest::Result<()> {
             + TOKEN;
     if !arg.history.clone() {
         let client = Client::new();
-        if let Some(custom) = arg.custom.clone() {
+        if let Some(mut custom) = arg.custom.clone() {
+            let s = custom.clone();
+            match arg.translate.clone() {
+                Some(Language::En) => custom = "「".to_string() + s.as_str() + "」を英語に翻訳して",
+                Some(Language::Jp) => {
+                    custom = "「".to_string() + s.as_str() + "」を日本語に翻訳して"
+                }
+                Some(Language::Fr) => {
+                    custom = "「".to_string() + s.as_str() + "」をフランス語に翻訳して"
+                }
+                _ => print!(""),
+            }
             let data = StdPostModel::new(custom.clone());
             let res = client
                 .post(url)
@@ -25,16 +39,21 @@ async fn main() -> reqwest::Result<()> {
                 .await?
                 .json::<ResponseModel>()
                 .await?;
-            let body = AppendToFile::new(custom,res.candidates[0].content.parts[0].text.clone());
-            let mut file_ref = OpenOptions::new().append(true).open("/Users/sotarofurukawa/.config/askai/value.txt").expect("Couldn't open file.");
-            file_ref.write_all(format!("{}",body).as_bytes()).expect("Couldn't write file.");
+            let body = AppendToFile::new(custom, res.candidates[0].content.parts[0].text.clone());
+            let mut file_ref = OpenOptions::new()
+                .append(true)
+                .open("/Users/sotarofurukawa/.config/askai/value.txt")
+                .expect("Couldn't open file.");
+            file_ref
+                .write_all(format!("{}", body).as_bytes())
+                .expect("Couldn't write file.");
             println!("{}", res.candidates[0].content.parts[0].text);
         }
-    }else {
+    } else {
         let mut file = File::open("/Users/sotarofurukawa/.config/askai/value.txt").unwrap();
         let mut body = String::new();
         file.read_to_string(&mut body).unwrap();
-        println!("{}",body);
+        println!("{}", body);
     }
     Ok(())
 }
